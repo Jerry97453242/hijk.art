dcd8fa0dd8e51e04e085f16d50cca086b931b959
Jerry97453242 <101013913+Jerry97453242@users.noreply.github.com>
2022-03-24 12:47:47 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -281,12 +281,13 @@ getData() {
             CERT_FILE="/usr/local/etc/xray/${DOMAIN}.pem"
             KEY_FILE="/usr/local/etc/xray/${DOMAIN}.key"
         else
-            resolve=`ping ${DOMAIN} -c 1 | sed ‘1{s/[^(]*(//;s/).*//;q}’`
-            res=`echo -n ${resolve} | grep ${IP}`
-            if [[ -z "${res}" ]]; then
-                colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
-                colorEcho ${RED}  " 域名未解析到当前服务器IP(${IP})!"
-                exit 1
+	    colorEcho ${BLUE}  "已默认${DOMAIN}设置成解析到本机"
+            #resolve=`ping ${DOMAIN} -c 1 | sed ‘1{s/[^(]*(//;s/).*//;q}’`
+            #res=`echo -n ${resolve} | grep ${IP}`
+            #if [[ -z "${res}" ]]; then
+            #    colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
+            #    colorEcho ${RED}  " 域名未解析到当前服务器IP(${IP})!"
+            #    exit 1
             fi
         fi
     fi
