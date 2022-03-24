e8e32dbf1f66e90f7f16d435812de1ebf727afd6
Jerry97453242 <101013913+Jerry97453242@users.noreply.github.com>
2022-03-24 12:49:21 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -272,12 +272,13 @@ getData() {
             CERT_FILE="/etc/v2ray/${DOMAIN}.pem"
             KEY_FILE="/etc/v2ray/${DOMAIN}.key"
         else
-            resolve=`ping ${DOMAIN} -c 1 | sed ‘1{s/[^(]*(//;s/).*//;q}’`
-            res=`echo -n ${resolve} | grep ${IP}`
-            if [[ -z "${res}" ]]; then
-                colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
-                colorEcho ${RED}  " 域名未解析到当前服务器IP(${IP})!"
-                exit 1
+	    colorEcho ${BLUE}  "已默认${DOMAIN}设置成解析到本机"
+            #resolve=`ping ${DOMAIN} -c 1 | sed ‘1{s/[^(]*(//;s/).*//;q}’`
+            #res=`echo -n ${resolve} | grep ${IP}`
+            #if [[ -z "${res}" ]]; then
+            #    colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
+            #    colorEcho ${RED}  " 域名未解析到当前服务器IP(${IP})!"
+            #    exit 1
             fi
         fi
     fi
